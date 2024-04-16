6cbaba1ca4f6dc7c6e2fbebb1b10726c2b304be3
Ashish Kumar <65803794+ashish15270@users.noreply.github.com>
2024-04-16 14:50:53 +0530

    Synchronizing job pytest from AWS Glue at 2024/04/16 09:20:53

---
+++ glue_ci_cd_stage/pytest.py
@@ -0,0 +1,26 @@
+import sys
+import pytest
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+## @params: [JOB_NAME]
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+@pytest.fixture
+def create_dataframe():
+    df = spark.read.parquet("s3://sampledata9873/mongo_smaple_data/*.parquet")
+    return df
+    
+def test_col_name():
+    assert "id" in df.columns
+    print("paased")
+job.commit()
